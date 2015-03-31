41dc5ab6a2a038acb03f44c3c4d95d05891cd79c
JimmyBlastoff <intrepidone@gmail.com>
2015-03-31 00:53:45 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+
+What is UnbrokenCoin ?
+
+
+UnbrokenCoin is an experimental new digital currency based on the Bitcoin Technology that enables instant payments to anyone, anywhere in the world. UnbrokenCoin uses peer-to-peer technology to operate with no central authority: managing transactions and issuing money are carried out collectively by the network. UnbrokenCoin Core is the name of open source software which enables the use of this currency
+
+Why did we create it ?
+
+i was involved in another Cryptocurrency project and that project ran into some design limitations that the Dev didn't want to change and when we asked him to we were told if we thought we could do it better we were more than welcome to try to do it ourselves, So we did.
+
+Specifictions 
+
+Unbrokencoin is a POW Sha256d
+Here is our answer to the 2016 BLock retargeting that we thought was too long for a Altcoin, So we have a 2 block retarget.
+
+We sped up the number of confirmations from 120 - 60 so that miners wouldn't have to wait for extended periods of time to receive there rewards.
+
+Fast Block times 120 seconds
+
+Halving Every Four years or 4000000 Blocks
+
+Total of 80 Million coins
+
+License
+
+Unbrokencoin is released under the terms of the MIT license. See COPYING for more information or see http://opensource.org/licenses/MIT.
+
+Unbrokencoin is based on Bitcoin's core with modifications to it's speed and size. Its development tracks Bitcoin's, the following information applies to Bitcoin's developemnt.
+
+
+Development process
+
+Developers work in their own trees, then submit pull requests when they think their feature or bug fix is ready.
+
+The patch will be accepted if there is broad consensus that it is a good thing. Developers should expect to rework and resubmit patches if they don't match the project's coding conventions (see coding.txt) or are controversial.
+
+The master branch is regularly built and tested, but is not guaranteed to be completely stable. Tags are regularly created to indicate new official, stable release versions of Unbrokencoin.
+
+Feature branches are created when there are major new features being worked on by several people.
+
+From time to time a pull request will become outdated. If this occurs, and the pull is no longer automatically mergeable; a comment on the pull will be used to issue a warning of closure. The pull will be closed 15 days after the warning if action is not taken by the author. Pull requests closed in this manner will have their corresponding issue labeled 'stagnant'.
+
+<<<<<<< HEAD
+Issues with no commits will be given a similar warning, and closed after 15 days from their last activity. Issues closed in this manner will be labeled 'stale'.
+=======
+Issues with no commits will be given a similar warning, and closed after 15 days from their last activity. Issues closed in this manner will be labeled 'stale'.
+>>>>>>> origin/master
---
+++ src/chainparams.cpp
@@ -0,0 +1,244 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "assert.h"
+
+#include "chainparams.h"
+#include "core.h"
+#include "protocol.h"
+#include "util.h"
+
+//
+// Main network
+//
+
+unsigned int pnSeed[] =
+{
+
+};
+
+class CMainParams : public CChainParams {
+public:
+    CMainParams() {
+        // The message start string is designed to be unlikely to occur in normal data.
+        pchMessageStart[0] = 0x64;
+        pchMessageStart[1] = 0x24;
+        pchMessageStart[2] = 0x08;
+        pchMessageStart[3] = 0x54;
+        nDefaultPort = 8546;
+        nRPCPort = 8546;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 20);
+        nSubsidyHalvingInterval = 4000000;
+
+        // Build the genesis block. Note that the output of the genesis coinbase cannot
+        // be spent as it did not originally exist in the database.
+  
+        const char* pszTimestamp = "Jim Blastoff releases UnbrokenCoin 28/03/2015";
+        CTransaction txNew;
+        txNew.vin.resize(1);
+        txNew.vout.resize(1);
+        txNew.vin[0].scriptSig = CScript() << 486604799 << CBigNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+        txNew.vout[0].nValue = 10 * COIN;
+        txNew.vout[0].scriptPubKey = CScript() << ParseHex("042e36d437b71fb7c3aaaf39d9979120bbabca600adf3d54554c69c9b62c79e1783b05a2e79c7d95a96168fb490aa0cadd2fd4ff908f0a5cc28d2e1e64f34df8f2") << OP_CHECKSIG;
+        genesis.vtx.push_back(txNew);
+        genesis.hashPrevBlock = 0;
+        genesis.hashMerkleRoot = genesis.BuildMerkleTree();
+        genesis.nVersion = 1;
+        genesis.nTime    = 1427576036;
+        genesis.nBits    = 0x1e0fffff;
+        genesis.nNonce   = 362709;
+        
+        //// debug print
+		//	hashGenesisBlock = genesis.GetHash();
+		//	while (hashGenesisBlock > bnProofOfWorkLimit.getuint256()){
+		//		if (++genesis.nNonce==0) break;
+		//		hashGenesisBlock = genesis.GetHash();
+        //}
+
+        printf("%s\n", hashGenesisBlock.ToString().c_str());
+        printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
+        printf("%x\n", bnProofOfWorkLimit.GetCompact());
+        genesis.print();
+
+        //This is the UNIQUE genhash information for Unbrokencoin made by Jimmy Blastoff
+        /*
+        00000c8c189093f5609910c2fd66e7dd31931f6d0ca9f3007788754e381436ca
+        e3aa8cc3f6de82349e3935ae170ae7d81b21100be07e9bca52ba8d8a568e60e1
+        1e0fffff
+        CBlock(hash=00000c8c189093f5609910c2fd66e7dd31931f6d0ca9f3007788754e381436ca, ver=1, hashPrevBlock=0000000000000000000000000000000000000000000000000000000000000000, hashMerkleRoot=e3aa8cc3f6de82349e3935ae170ae7d81b21100be07e9bca52ba8d8a568e60e1, nTime=1427576036, nBits=1e0fffff, nNonce=362709, vtx=1)
+          CTransaction(hash=e3aa8cc3f6, ver=1, vin.size=1, vout.size=1, nLockTime=0)
+            CTxIn(COutPoint(0000000000, 4294967295), coinbase 04ffff001d01042d4a696d20426c6173746f66662072656c656173657320556e62726f6b656e436f696e2032382f30332f32303135)
+            CTxOut(nValue=10.00000000, scriptPubKey=042e36d437b71fb7c3aaaf39d99791)
+          vMerkleTree: e3aa8cc3f6de82349e3935ae170ae7d81b21100be07e9bca52ba8d8a568e60e1
+         */
+        
+        
+        assert(hashGenesisBlock == uint256("0x00000c8c189093f5609910c2fd66e7dd31931f6d0ca9f3007788754e381436ca"));
+        assert(genesis.hashMerkleRoot == uint256("0xe3aa8cc3f6de82349e3935ae170ae7d81b21100be07e9bca52ba8d8a568e60e1"));
+
+        vSeeds.push_back(CDNSSeedData("", "")); //This is where Jimmy put his seednode so no need to do addnode <someip> add bullshit for 8 months...
+
+
+        base58Prefixes[PUBKEY_ADDRESS] = 68; //This is where Jimmy changed the starting letter for Unbrokencoin addresses so that they are different than Bitcoins!
+        base58Prefixes[SCRIPT_ADDRESS] = 30;
+        base58Prefixes[SECRET_KEY] = 196;
+
+        // Convert the pnSeeds array into usable address objects.
+        for (unsigned int i = 0; i < ARRAYLEN(pnSeed); i++)
+        {
+            // It'll only connect to one or two seed nodes because once it connects,
+            // it'll get a pile of addresses with newer timestamps.
+            // Seed nodes are given a random 'last seen time' 
+            const int64 nTwoDays = 2 * 24 * 60 * 60;
+            struct in_addr ip;
+            memcpy(&ip, &pnSeed[i], sizeof(ip));
+            CAddress addr(CService(ip, GetDefaultPort()));
+            addr.nTime = GetTime() - GetRand(nTwoDays) - nTwoDays;
+            vFixedSeeds.push_back(addr);
+        }
+    }
+
+    virtual const CBlock& GenesisBlock() const { return genesis; }
+    virtual Network NetworkID() const { return CChainParams::MAIN; }
+
+    virtual const vector<CAddress>& FixedSeeds() const {
+        return vFixedSeeds;
+    }
+protected:
+    CBlock genesis;
+    vector<CAddress> vFixedSeeds;
+};
+static CMainParams mainParams;
+
+
+//
+// Testnet (v3)
+//
+class CTestNetParams : public CMainParams {
+public:
+    CTestNetParams() {
+        // The message start string is designed to be unlikely to occur in normal data.
+        pchMessageStart[0] = 0x01;
+        pchMessageStart[1] = 0xfe;
+        pchMessageStart[2] = 0xfe;
+        pchMessageStart[3] = 0x05;
+        nDefaultPort = 55534;
+        nRPCPort = 55535;
+        strDataDir = "testnet";
+
+        // Modify the testnet genesis block so the timestamp is valid for a later start.
+        genesis.nTime = 1374901773;
+        genesis.nNonce = 1211565;
+        
+        
+        //// debug print
+        hashGenesisBlock = genesis.GetHash();
+        //while (hashGenesisBlock > bnProofOfWorkLimit.getuint256()){
+        //    if (++genesis.nNonce==0) break;
+        //   hashGenesisBlock = genesis.GetHash();
+        //}
+
+        printf("%s\n", hashGenesisBlock.ToString().c_str());
+        printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
+        genesis.print();
+
+        vFixedSeeds.clear();
+        vSeeds.clear();
+        // vSeeds.push_back(CDNSSeedData("unbrokencoin.test", "test.unbrokencoin.org"));
+
+        base58Prefixes[PUBKEY_ADDRESS] = 130;
+        base58Prefixes[SCRIPT_ADDRESS] = 30;
+        base58Prefixes[SECRET_KEY] = 239;
+
+    }
+    virtual Network NetworkID() const { return CChainParams::TESTNET; }
+};
+static CTestNetParams testNetParams;
+
+
+//
+// Regression test
+//
+class CRegTestParams : public CTestNetParams {
+public:
+    CRegTestParams() {
+        pchMessageStart[0] = 0xfa;
+        pchMessageStart[1] = 0x0f;
+        pchMessageStart[2] = 0xa5;
+        pchMessageStart[3] = 0x5a;
+        nSubsidyHalvingInterval = 150;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 1);
+        genesis.nTime = 1296688602;
+        genesis.nBits = 0x207fffff;
+        genesis.nNonce = 3;
+        hashGenesisBlock = genesis.GetHash();
+        nDefaultPort = 18444;
+        strDataDir = "regtest";
+        
+        //// debug print
+        hashGenesisBlock = genesis.GetHash();
+        //while (hashGenesisBlock > bnProofOfWorkLimit.getuint256()){
+        //    if (++genesis.nNonce==0) break;
+        //    hashGenesisBlock = genesis.GetHash();
+        //}
+
+        printf("%s\n", hashGenesisBlock.ToString().c_str());
+        printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
+        genesis.print();
+
+        // assert(hashGenesisBlock == uint256("0x13d8d31dde96874006da503dd2b63fa68c698dc823334359e417aa3a92f80433"));
+
+        vSeeds.clear();  // Regtest mode doesn't have any DNS seeds.
+
+        base58Prefixes[PUBKEY_ADDRESS] = 0;
+        base58Prefixes[SCRIPT_ADDRESS] = 5;
+        base58Prefixes[SECRET_KEY] = 128;
+    }
+
+    virtual bool RequireRPCPassword() const { return false; }
+    virtual Network NetworkID() const { return CChainParams::REGTEST; }
+};
+static CRegTestParams regTestParams;
+
+static CChainParams *pCurrentParams = &mainParams;
+
+const CChainParams &Params() {
+    return *pCurrentParams;
+}
+
+void SelectParams(CChainParams::Network network) {
+    switch (network) {
+        case CChainParams::MAIN:
+            pCurrentParams = &mainParams;
+            break;
+        case CChainParams::TESTNET:
+            pCurrentParams = &testNetParams;
+            break;
+        case CChainParams::REGTEST:
+            pCurrentParams = &regTestParams;
+            break;
+        default:
+            assert(false && "Unimplemented network");
+            return;
+    }
+}
+
+bool SelectParamsFromCommandLine() {
+    bool fRegTest = GetBoolArg("-regtest", false);
+    bool fTestNet = GetBoolArg("-testnet", false);
+
+    if (fTestNet && fRegTest) {
+        return false;
+    }
+
+    if (fRegTest) {
+        SelectParams(CChainParams::REGTEST);
+    } else if (fTestNet) {
+        SelectParams(CChainParams::TESTNET);
+    } else {
+        SelectParams(CChainParams::MAIN);
+    }
+    return true;
+}
